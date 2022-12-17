5ec845a19b36814442fd458c79fd0f5d3758f752
yudavinchi <davidyu@edu.hac.ac.il>
2022-12-17 15:07:31 +0200

    working register and login

--- src/main/java/calendar/SpringApp.java
+++ src/main/java/calendar/SpringApp.java
@@ -2,12 +2,11 @@ package calendar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class SpringApp {
     public static void main(String[] args) {
         SpringApplication.run(SpringApp.class, args);
     }
-
 }
