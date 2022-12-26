2a4db4c59717306c0778e1481dc5a35728875340
Sedri14 <sedri14@gmail.com>
2022-12-26 13:50:08 +0200

    changed mathod sharedCalendarByMonth in EventController to return a map eventsByEmail for each user that shared his events with me

---
+++ src/main/java/calendar/DTO/EmailList.java
@@ -0,0 +1,19 @@
+package calendar.DTO;
+
+import java.util.List;
+
+public class EmailList {
+
+    private List<String> emails;
+
+    public List<String> getEmails() {
+        return emails;
+    }
+
+    public void setEmails(List<String> emails) {
+        this.emails = emails;
+    }
+}
+
+
+
