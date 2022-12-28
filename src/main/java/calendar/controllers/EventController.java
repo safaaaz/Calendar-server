70c868511b21665f9a8577711f3b1a6691b8edf3
yudavinchi <yudin.david@gmail.com>
2022-12-28 14:00:46 +0200

    event controller commented

--- src/main/java/calendar/controllers/EventController.java
+++ src/main/java/calendar/controllers/EventController.java
@@ -42,11 +42,27 @@ public class EventController {
 
     public static final Logger logger = LogManager.getLogger(EventController.class);
 
+    /**
+     * Fetches an existing event by its ID.
+     * @param eventId The ID of the event to fetch.
+     * @return An object representing the fetched event.
+     */
     @RequestMapping(value = "findOne/{eventId}", method = RequestMethod.GET)
     public ResponseEntity<Event> fetchEventById(@PathVariable Long eventId) {
         return ResponseEntity.ok(eventService.fetchEventById(eventId));
     }
 
+
+
+    /**
+     * This function adds a new event based on the CreateEventDTO object, and the user token provided in the request header.
+     * It checks that the title and dateTime are provided and throws a MissingEventFieldException if either are missing.
+     * The function then gets the user from the token and returns the response entity containing the added event.
+     *
+     * @param createEventDTO The CreateEventDTO object containing the details of the event to add
+     * @param token The token of the user who is adding the event
+     * @return A response entity containing the added event
+     */
     @RequestMapping(value = "add", method = RequestMethod.POST)
     public ResponseEntity<Event> addEvent(@RequestBody CreateEventDTO createEventDTO, @RequestHeader("token") String token) {
 
@@ -64,6 +80,13 @@ public class EventController {
         return ResponseEntity.ok(eventService.add(createEventDTO, organizer));
     }
 
+    /**
+     * Retrieves a list of events belonging to the user provided in the token parameter for the month specified in the month parameter.
+     *
+     * @param token   the user's token
+     * @param month   the month number to retrieve events for
+     * @return        ResponseEntity<List<Event>> containing the list of events
+     */
     @RequestMapping(value = "myEventsByMonth/{month}", method = RequestMethod.GET)
     public ResponseEntity<List<Event>> getMyEventsByMonth(@RequestHeader(value="token") String token, @PathVariable int month){
         logger.info("In get events by month function");
@@ -74,8 +97,12 @@ public class EventController {
         return ResponseEntity.ok(eventService.getEventsByMonth(user,month));
     }
 
-    //This method gets all user events (private events excluded), if this
-    //user has shared his calendar with me.
+    /**
+     * This function retrieves events for the user with the given token for the given month.
+     * @param token The token of the user who wants to retrieve his events
+     * @param month The month for which the events should be retrieved
+     * @return ResponseEntity<Map<String,List<Event>>> a response entity containing a list of events for the given month
+     */
     @RequestMapping(value = "share/sharedCalendars/{month}", method = RequestMethod.POST)
     public ResponseEntity<Map<String,List<Event>>> sharedCalendarByMonth(@RequestHeader("token") String token, @PathVariable int month, @RequestBody EmailList emailList) {
         //TODO: check month number is legal(1-12)
@@ -91,6 +118,12 @@ public class EventController {
         return ResponseEntity.ok(eventService.getSharedCalendarByMonth(user,others,month));
     }
 
+    /**
+     * This function returns a list of users associated with the event that the specified user has shared.
+     *
+     * @param token the authentication token of the user
+     * @return a ResponseEntity containing a list of UserDTOs associated with the event that the specified user has shared
+     */
     @RequestMapping(value = "share/shareList", method = RequestMethod.GET)
     public ResponseEntity<List<UserDTO>> shareList(@RequestHeader("token") String token) {
 
@@ -100,37 +133,38 @@ public class EventController {
         return ResponseEntity.ok(eventService.shareList(user));
     }
 
-//    @RequestMapping(value = "myEventsByMonth/{month}", method = RequestMethod.GET)
-//    public ResponseEntity<Map<String,List<Event>>> getEventsByMonth(@RequestHeader(value="token") String token, @PathVariable int month){
-//        logger.info("In get events by month function");
-//        logger.info("user with token: " + token + " want to get his events for month " + month);
-//        User user = authService.getCachedUser(token);
-//        user = userService.fetchUserById(user.getId());             //Why getting the user twice?
-//
-//        Map<String,List<Event>> calendarsByEmail = new HashMap<>();
-//        List<Event> eventsAsOrganizer = eventService.getEventsByMonth(user, month);
-//        calendarsByEmail.put(user.getEmail(), eventsAsOrganizer);
-//
-//        //Collect all shared calendars data into a map (exclude all private events)
-//        for (User u: user.getMySharedWithCalendars()) {
-//            List<Event> publicEvents = eventService.getEventsByMonth(u, month).stream().filter(event -> event.isPrivate() == false).collect(Collectors.toList());
-//            calendarsByEmail.put(u.getEmail(), publicEvents);
-//        }
-//
-//        return ResponseEntity.ok(calendarsByEmail);
-//    }
+    /**
+     * Deletes the event with the specified eventId
+     *
+     * @param eventId Long - ID of the event to delete
+     *
+     * @return ResponseEntity<String> - An HTTP response with the status of the operation
+     */
     @RequestMapping(value = "deleteEventById/{eventId}", method = RequestMethod.DELETE)
     public ResponseEntity<String> getEventsByMonth(@PathVariable Long eventId){
         logger.info("In delete events by id function");
         return ResponseEntity.ok(eventService.deleteEventById(eventId));
     }
 
+    /**
+     * This function is used to delete an event using its id.
+     *
+     * @param eventId The eventId of the event to be deleted
+     * @return ResponseEntity<String> An HTTP response containing the result of the delete operation
+     */
     @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
     public ResponseEntity<String> deleteEventById(@RequestHeader("eventId") Long eventId){
         logger.info("In delete event by its id");
         return ResponseEntity.ok(eventService.deleteEventById(eventId));
     }
 
+    /**
+     * Invite a guest to the event
+     *
+     * @param eventId the id of the event to invite the guest to
+     * @param userDTO the userDTO with the guest's email
+     * @return ResponseEntity<UserDTO> of the invited guest
+     */
     @RequestMapping(value = "inviteGuest", method = RequestMethod.POST)
     public ResponseEntity<UserDTO> inviteGuest(@RequestHeader Long eventId, @RequestBody UserDTO userDTO) {
         if (isBlank(userDTO.email)) {
@@ -142,6 +176,15 @@ public class EventController {
 
         return ResponseEntity.ok(eventService.inviteGuest(event, guest));
     }
+
+    /**
+     * This function removes a guest from an event based on the eventId and UserDTO.
+     *
+     * @param eventId the id of the event
+     * @param userDTO the UserDTO containing the email of the guest
+     * @return the User that was removed
+     * @throws IllegalArgumentException if the guest email is blank
+     */
     @RequestMapping(value = "removeGuest/{eventId}", method = RequestMethod.POST)
     public ResponseEntity<User> removeGuest(@PathVariable Long eventId, @RequestBody UserDTO userDTO) {
         if (isBlank(userDTO.email)) {
@@ -154,6 +197,15 @@ public class EventController {
         return ResponseEntity.ok(eventService.removeGuest(event, guest));
     }
 
+
+    /**
+     * This function handles the request to update an event.
+     *
+     * @param updateEventDTO The UpdateEventDTO object containing the information about the event to be updated.
+     * @param token The token of the user associated with the event.
+     *
+     * @return A ResponseEntity containing a ResponseUpdatedEvent object.
+     */
     @RequestMapping(value = "/update", method = RequestMethod.POST)
     public ResponseEntity<ResponseUpdatedEvent> updateEvent(@RequestBody UpdateEventDTO updateEventDTO, @RequestHeader("token") String token) {
 
@@ -168,13 +220,12 @@ public class EventController {
 
         User user = authService.getCachedUser(token);
 
-        // lets assume everything is okay, thats is why we call the function
-        // in general we should wait till the update is finished and then send notification
-
         notificationService.sendUpdateEventNotification(updateEventDTO);
 
         return ResponseEntity.ok(eventService.updateEvent(updateEventDTO, user));
     }
+
+
     public static class ResponseUpdatedEvent{
         private Long eventId;
         private String eventTitle;
@@ -207,6 +258,15 @@ public class EventController {
             return editorEmail;
         }
     }
+
+
+    /**
+     * Makes a User an Admin of an Event.
+     *
+     * @param userDTO A UserDTO object containing the email address of the User to be made an Admin.
+     * @param eventId The unique identifier of the Event the User is to be made an Admin of.
+     * @return A ResponseEntity object containing the UserDTO object of the User made an Admin.
+     */
     @RequestMapping(value = "/makeAdmin", method = RequestMethod.POST)
     public ResponseEntity<UserDTO> makeAdmin(@RequestBody UserDTO userDTO, @RequestHeader("eventId") Long eventId) {
         if (isBlank(userDTO.email)) {
